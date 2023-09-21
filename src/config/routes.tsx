582ddaca1b24ddfb41c526931b9812f84dfc9cb7
Adedayo Jiboye <60001148+dayojiboye@users.noreply.github.com>
2023-09-21 09:04:27 +0100

    Initial commit

---
+++ src/config/routes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+import { RootStackParamList } from "../types";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppRoutes() {
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false, gestureEnabled: true }}>
+			<Stack.Screen name="Home" component={Home} />
+		</Stack.Navigator>
+	);
+}
